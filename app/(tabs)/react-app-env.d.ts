c7a10db6ea01da156bb47846c3621fc0e7182500
Kai <174383617+Koiwingy@users.noreply.github.com>
2026-03-21 09:32:04 -0400

    Create react-app-env.d.ts
    Allowing the access of the files in the assets/images folder

---
+++ app/(tabs)/react-app-env.d.ts
@@ -0,0 +1,5 @@
+//Fixing problme with getting pngs,jpgs, etc from the images folder, since currently wont allow
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
