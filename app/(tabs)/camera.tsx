04e19d053706f762bd767ae4a7c63efb18cef008
Kai <174383617+Koiwingy@users.noreply.github.com>
2026-03-23 11:00:49 -0400

    Update and intital ai implementation to camera.tsx
    Initial implementation for ai component. Just boiler plate and will change later on

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,24 +1,29 @@
-import { Image } from "expo-image";
-import { Text, View, StyleSheet, Button, Platform, TouchableOpacity } from "react-native";
-import React, { ReactNode, useRef } from "react";
+import React from "react";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
+//navigation from file
+import { useRouter } from 'expo-router';
 
 //camera functionality
-import { CameraView, Camera} from 'expo-camera';
+import { Camera, CameraView } from 'expo-camera';
 
 //useState allows storing vars inside componenets
 //useEffect performs some action through useState
 //Most tutorials use this to store the data
-import {useState, useEffect} from "react";
+import { useEffect, useState } from "react";
 
+//ai component
 
-
-
+//pasing it into the ai
+let barcodeNumber = "brochacho";
 
 const Scanner = () => {
-  
-const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-const [scanData, setScanData] = useState(false);
+  //router for links
+  const router = useRouter();
+  //permisions and the scan data
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanData, setScanData] = useState(false);
+  //sharing and using barcodenumber as a var in the text, since it'll change
 
   useEffect(() =>{
     //Request perms for the camera
@@ -47,8 +52,6 @@ const [scanData, setScanData] = useState(false);
 
   //To stop it after getting the barcode, uses a useRef:
   //Off when ready, on to stop further scans through handleBarCodeScanned
-
-
   
   const handleBarCodeScanned = ({type, data}: { type: any, data: any }) => {
     setScanData(true);
@@ -56,32 +59,54 @@ const [scanData, setScanData] = useState(false);
     console.log("Type: " + type)
     console.log("Data: " + data)
 
-    const barcodeNumber = data;
     //logic here to process the barcode number into the ai api
-    
+    barcodeNumber = data
+    // main();
+  }; 
+
+  // const api_key = "<INSERT API KEY HERE FOR AI POOKIE>"
+
+  //used to initialize the google ai sdk
+//   const ai = new GoogleGenAI({apiKey: api_key});
+  
+//   async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     contents: "Explain how AI works in a few words",
+//   });
+//   console.log(response.text);
+// }
+
+  //pass it to ai, since it'll now have the barcode var saved
+  const ai = () => {
+    alert("Accessed barcode data: " + barcodeNumber);
+    //functionality of the ai
   };
 
-  //Buttons for ending and starting camera again
-
-
-  
 
 
   return (
     <View style = {styles.container}>
+    <View style = {styles.cameraContainer}>
       <CameraView
         style = {StyleSheet.absoluteFillObject}
-        //For web, However, it only works for qr codes. Unless the camera on my device is terrible.
         barcodeScannerSettings={{ barcodeTypes: ["ean13", "qr", "ean8", "aztec", "upc_a", "upc_e", "codabar", "code39", "datamatrix", "itf14", "pdf417"],}} 
-
         onBarcodeScanned= {scanData? undefined:handleBarCodeScanned}
         />
         {scanData && <Button 
-          title="Press To Open Camera to Scan"
+          title="Clck To Scan Again"
           onPress={()=>setScanData(false)}
+          color="#241584"
         /> }
+        <Button 
+        title="Click to go Ai page"
+        onPress={() => router.push("/ai")}>
+        </Button>        
+        <Button title="ai" onPress={ai} />
                 
       </View>
+
+    </View>
   );
 }
 
@@ -92,6 +117,11 @@ const styles = StyleSheet.create ({
     justifyContent: 'center',
     alignItems: 'center',
     paddingLeft: 30,
+  },
+  cameraContainer: {
+    width: 500,  
+    height: 500, 
+    overflow: 'hidden',
   },
   images: {
     width: 100,
@@ -105,7 +135,13 @@ const styles = StyleSheet.create ({
   },
   text: {
     fontFamily: 'Arial',
-    fontSize: 20,
+    color: '#8000ff',
+    fontSize: 40,
+    //the camera background is naturally black, so cant do dark bg or that
+    backgroundColor: '#95ff00',
+    padding: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 })
 
